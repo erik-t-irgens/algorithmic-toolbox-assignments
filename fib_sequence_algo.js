b1058cda5c9f67eedba88169e4a536b70d573d5f
Erik Irgens <erik.t.irgens@gmail.com>
2020-11-18 22:10:21 -0800

    Update fib_sequence_algo.js
    Added second function that is more efficient, not computing the same result multiple times like in the recursive solution.

--- fib_sequence_algo.js
+++ fib_sequence_algo.js
@@ -1,6 +1,18 @@
+// Slow, recursive, function.
+
 function fib(n) {
     if (n <= 1) {
         return n
     }
     return fib(n - 1) + fib(n - 2)
+}
+// Faster functional algorithm
+
+function fibEff(n) {
+    let fibArr = [0, 1]
+    for (let i = 2; i < n; i++) {
+        fibArr.push(0)
+        fibArr[i] = fibArr[i - 1] + fibArr[i - 2]
+    }
+    return fibArr[n - 1]
 }
